6d76f6cc082e93c1fb4870592b1725f21b53d3e4
Yubraj Timilsina <yubrajtimilsina32@gmail.com>
2024-11-20 15:19:24 +0545

    login

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,7 +1,7 @@
 
 import { Request,Response } from "express";
 import User from "../Database/models/userModels";
-import sequelize  from "../Database/connection";
+import bcrypt from 'bcrypt'
 
 
 class UserController{
@@ -21,13 +21,49 @@ class UserController{
         await User.create({
             username,
             email,
-            password // Stored directly without hashing
+            password :bcrypt.hashSync(password,10), // Stored directly without hashing
         });
     
         // Respond with a success message
         res.status(201).json({
             message: "User registered successfully"
         });
+    }
+
+    static async login(req:Request,res:Response){
+        //accept incoming data ---> email, password
+        const {email, password} =req.body
+        if(!email || !password){
+            res.status(200).json({
+                message : "please provide email, password"
+            })
+            return
+        }
+        //check email exixt on a table or not 
+        const [user] = await User.findAll({ //find -->findAll---> Data in array
+            where :{
+                email : email
+            }
+        })
+        //data return in array
+       
+        if(!user){
+            res.status(404).json({
+                message : "No user with this👌😁"
+            })
+        }else{
+            // //if yes -->email exist-->check password
+            const isEqual = bcrypt.compareSync(password,user.password)
+            if(!isEqual){
+                res.status(400).json({
+                    message : "Invalid password😒"
+                })
+            }else{
+                res.status(200).json({
+                    message : "logged in sucessfully👌"
+                })
+            }
+        }
     }
 }
 
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -4,5 +4,5 @@ import UserController from "../controllers/userController";
 const router = Express.Router()
 
 router.route("/register").post(UserController.register)
-
+router.route("/login").post(UserController.login)
 export default router
