b21b7e805e7f8d5144633883fa6bfe7b8b6d208b
Yubraj Timilsina <yubrajtimilsina32@gmail.com>
2024-11-26 18:37:53 +0545

    error handling

--- src/routes/productRoute.ts
+++ src/routes/productRoute.ts
@@ -3,19 +3,20 @@ import express, {Router} from 'express'
 import productController from '../controllers/productController'
 import userMiddleware, { Role } from '../middleware/userMiddleware'
 import {multer,storage} from '../middleware/multerMiddleware'
+import errorHandler from '../services/errorHandler'
 
 const upload = multer({storage : storage})
 
 const router:Router = express.Router()
 
 router.route("/")
-.post(userMiddleware.isUserLoggedIn,userMiddleware.accessTo(Role.Admin), upload.single("productImage"), productController.createProduct)
+.post(userMiddleware.isUserLoggedIn,userMiddleware.accessTo(Role.Admin), upload.single("productImage"), errorHandler(productController.createProduct))
 .get(productController.getAllProducts)
 
 router.route("/:id")
-.post(userMiddleware.isUserLoggedIn,userMiddleware.accessTo(Role.Admin), productController.createProduct)
-.get(productController.getSingleProducts)
-.delete(userMiddleware.isUserLoggedIn, userMiddleware.accessTo(Role.Admin),productController.deleteProducts)
+.post(userMiddleware.isUserLoggedIn,userMiddleware.accessTo(Role.Admin), errorHandler(productController.deleteProducts))
+.get(errorHandler(productController.getSingleProducts))
+.delete(userMiddleware.isUserLoggedIn, userMiddleware.accessTo(Role.Admin), errorHandler (productController.deleteProducts))
 
 
 export default router
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,11 +1,12 @@
 import Express, { Router }  from "express";
 import UserController from "../controllers/userController";
+import errorHandler from "../services/errorHandler";
 
 const router:Router = Express.Router()
 
-router.route("/register").post(UserController.register)
-router.route("/login").post(UserController.login)
-router.route("/forgot-password").post(UserController.handleForgotPassword)
-router.route("/verify-otp").post(UserController.verifyOtp)
-router.route("/reset-password").post(UserController.resetPassword)
+router.route("/register").post(errorHandler(UserController.register))
+router.route("/login").post(errorHandler(UserController.login))
+router.route("/forgot-password").post(errorHandler(UserController.handleForgotPassword))
+router.route("/verify-otp").post(errorHandler(UserController.verifyOtp))
+router.route("/reset-password").post(errorHandler(UserController.resetPassword))
 export default router
