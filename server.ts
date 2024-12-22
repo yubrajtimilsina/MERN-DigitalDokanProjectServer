e88930d536a1d0d66ee3c193f44cc7aecbcc7231
Yubraj Timilsina <yubrajtimilsina32@gmail.com>
2024-12-22 12:14:44 +0545

    socket io

--- server.ts
+++ server.ts
@@ -1,57 +1,76 @@
-import app from "./src/app";
-import sequelize from "./src/Database/connection";
-
-import {envConfig } from "./src/config/config"
-import { config } from "dotenv"
 import adminSeeder from "./adminSeeder";
-import categoryControllers from "./src/controllers/categoryControllers";
-import {Server} from 'socket.io';
-import  jwt from "jsonwebtoken"
+import app from "./src/app";
+
+import { envConfig } from "./src/config/config";
+import categoryController from "./src/controllers/categoryControllers";
+import { Server } from "socket.io";
+import jwt from 'jsonwebtoken'
 import User from "./src/Database/models/userModels";
+import Order from "./src/Database/models/orderModel";
 
 function startServer(){
     const port = envConfig.port || 4000
-    const server = app.listen(envConfig.port, () => {
-        categoryControllers.seedCategory()
-    console.log('Server has started at port[${port}]')
-    adminSeeder()
-})
-const io = new Server(server,{
-    cors : {
-        origin : 'http://localhost:5173'
+    const server = app.listen(port,()=>{
+        categoryController.seedCategory()
+        console.log(`Server has started at port [${port}]`)
+        adminSeeder()
+    })
+    const io = new Server(server,{
+        cors : {
+            origin : 'http://localhost:5173'
+        }
+    })
+    let onlineUsers:{socketId:string,userId:string,role:string}[] = []
+    let addToOnlineUsers = (socketId:string,userId:string,role:string)=>{
+        onlineUsers = onlineUsers.filter((user)=>user.userId !== userId) 
+        onlineUsers.push({socketId,userId,role})
     }
-})
-
-let onlineUsers:{socketId:string,userId:string,role:string}[] = []
-let addToOnlineUsers = (socketId:string,userId:string,role:string)=>{
-    onlineUsers = onlineUsers.filter((user)=>user.userId !== userId) 
-    onlineUsers.push({socketId,userId,role})
-}
-
-
-
-io.on("connection",(socket)=>{
-    const {token} = socket.handshake.auth // jwt token 
-    if(token){
-        jwt.verify(token,envConfig.jwtSecretKey as string, async (err:any,result:any)=>{
-            if(err){
-                socket.emit("error",err)
-            }else{
-                const userData = await User.findByPk(result.userId) // {email:"",pass:"",role:""}
-                if(!userData){
-                   socket.emit("error","No user found with that token")
-                    return
+    io.on("connection",(socket)=>{
+        const token = socket.handshake.headers.token // jwt token 
+        if(token){
+            jwt.verify(token as string,envConfig.jwtSecretKey as string, async (err:any,result:any)=>{
+                if(err){
+                    socket.emit("error",err)
+                }else{
+                    const userData = await User.findByPk(result.userId) // {email:"",pass:"",role:""}
+                    if(!userData){
+                       socket.emit("error","No user found with that token")
+                        return
+                    }
+                    // userID grab garnu paryo 
+                    // 2, 2, customer
+                    addToOnlineUsers(socket.id,result.userId,userData.role)
+                    console.log(onlineUsers)
+              
                 }
-                // userID grab garnu paryo 
-                // 2, 2, customer
-                addToOnlineUsers(socket.id,result.userId,userData.role)
-          
+               })
+               
+        }else{
+            socket.emit("error","Please provide token")
+        }
+        socket.on("updateOrderStatus",async (data)=>{
+            const {status,orderId,userId} = data
+            console.log(status,orderId)
+            const findUser = onlineUsers.find(user=>user.userId == userId) // {socketId,userId, role}
+            await Order.update(
+                {
+                    orderStatus : status
+                },
+               {
+                 where : {
+                    id : orderId
+                }
+               } 
+            )
+            if(findUser){
+                io.to(findUser.socketId).emit("success","Order Status updated successfully!!")
+            }else{
+                socket.emit("error","User is not online!!")
             }
-           })
-           
-    }
-})
+        })
+    })
 
 }
+
 
 startServer()
